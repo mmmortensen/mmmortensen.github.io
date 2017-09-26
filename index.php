f81ebe8ba99e4e7732ddbb3711f39f4a498f6538
mmmortensen <mmmortensen@users.noreply.github.com>
2017-09-26 04:39:57 +0200

    temporary portfolio
    Temporary for learning puposes

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include 'includes/head.php';?>
+    <header>
+      <div class="header">
+        Portfolio :: Mikkel Magdal Mortensen
+      </div>
+    </header>
+    <h1>Det her er h1</h1>
+    <p>brødtekst</p>
+    <nav>
+      <ul>
+        <li>bullet point</li>
+        <li>bullet point 2</li>
+        <a href="learning.html"><li>Learning</li></a>
+        <div class="info">
+
+        </div>
+      </ul>
+    </nav>
+    <img src="images/awesomo.gif" alt="A.W.E.S.O.M.O" title="A.W.E.S.O.M.-O">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hSlb1ezRqfA" frameborder="0" allowfullscreen></iframe>
+    </video>
+
+    <footer>
+      <div class="footer">
+        <p>2017</p>
+        <h1>HEJ HEJ H1</h1>
+      </div>
+    </footer>
+  </body>
+</html>
